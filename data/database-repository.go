8c1cbcf36e94ff661bd97020d4fde45fc15cd7f4
Victorine Osewe <vosewe@powergen-re.com>
2020-09-13 21:26:26 +0300

    adding bycrpt

--- data/database-repository.go
+++ data/database-repository.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserRepository struct {
@@ -16,12 +17,19 @@ func NewUserRepository(database *gorm.DB) UserRepository {
 }
 
 func (repo UserRepository) CreateUser(user registering.Users) (userID uuid.UUID, erro error) {
+	// Salt and hash the password using the bcrypt algorithm
+	// The second argument is the cost of hashing, which we arbitrarily set as 8 (this value can be more or less, depending on the computing power you wish to utilize)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
 	User := User{
 		ID:         newUUID(),
 		Firstname:  user.Firstname,
 		Lastname:   user.Lastname,
 		Email:      user.Email,
-		Password:   user.Password,
+		Password:   string(hashedPassword),
 		Gender:     user.Gender,
 		Jobrole:    user.Jobrole,
 		Department: user.Department,
